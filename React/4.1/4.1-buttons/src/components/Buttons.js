10a34f96899a8a456b1beb17d5f294b68ad99927
Pandad89 <adambiren19@gmail.com>
2021-12-13 15:54:18 +0200

    13.12.21

---
+++ React/4.1/4.1-buttons/src/components/Buttons.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Buttons = () => {
+    return (
+        <div className="Buttons">
+            <button className="FirstButton">
+                Important
+            </button>
+            <button className="SecondButton">
+                Not Important
+            </button>
+        </div>
+    )
+}
+
+export default Buttons;
